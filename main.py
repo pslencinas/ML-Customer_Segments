0899911f4fb8b2dc4dcfd1cc3b85152960fc253d
Pablo Lencinas <pslencinas@gmail.com>
2018-06-25 00:06:16 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,185 @@
+# Import libraries necessary for this project
+import numpy as np
+import pandas as pd
+from IPython.display import display # Allows the use of display() for DataFrames
+
+import warnings
+warnings.filterwarnings('ignore')
+
+# Import supplementary visualizations code visuals.py
+import visuals as vs
+
+# Pretty display for notebooks
+#%matplotlib inline
+
+# Load the wholesale customers dataset
+try:
+    data = pd.read_csv("customers.csv")
+    data.drop(['Region', 'Channel'], axis = 1, inplace = True)
+    print("Wholesale customers dataset has {} samples with {} features each.".format(*data.shape))
+except:
+    print("Dataset could not be loaded. Is the dataset missing?")
+
+# Display a description of the dataset
+display(data.describe())
+
+# TODO: Select three indices of your choice you wish to sample from the dataset
+indices = [1, 10, 100]
+
+# Create a DataFrame of the chosen samples
+samples = pd.DataFrame(data.loc[indices], columns = data.keys()).reset_index(drop = True)
+print("Chosen samples of wholesale customers dataset:")
+display(samples)
+
+# Imports
+from sklearn.cross_validation import train_test_split
+from sklearn.tree import DecisionTreeRegressor
+
+columns = list(data.columns)
+
+for column in columns:
+    # TODO: Make a copy of7 the DataFrame, using the 'drop' function to drop the given feature
+    new_data = data.drop(column, axis = 1 )
+    #display(new_data.head(2))
+    new_feature = pd.DataFrame(data.loc[:, column])
+    #display(new_feature.head(2))
+    
+    # TODO: Split the data into training and testing sets(0.25) using the given feature as the target
+    # Set a random state.
+    X_train, X_test, y_train, y_test = train_test_split(new_data, new_feature, test_size=0.25, random_state=30)
+    
+    # TODO: Create a decision tree regressor and fit it to the training set
+    regressor = DecisionTreeRegressor(random_state=30)
+    regressor.fit(X_train, y_train)
+    
+    # TODO: Report the score of the prediction using the testing set
+    score = regressor.score(X_test, y_test)
+    print('R2 score for {} as dependent variable: {}'.format(column, score))
+    
+# Produce a scatter matrix for each pair of features in the data
+pd.scatter_matrix(data, alpha = 0.3, figsize = (14,8), diagonal = 'kde');
+
+# TODO: Scale the data using the natural logarithm
+log_data = np.log(data)
+
+# TODO: Scale the sample data using the natural logarithm
+log_samples = np.log(samples)
+
+# Produce a scatter matrix for each pair of newly-transformed features
+pd.scatter_matrix(log_data, alpha = 0.3, figsize = (14,8), diagonal = 'kde');
+
+# Display the log-transformed sample data
+display(log_samples)
+
+# For each feature find the data points with extreme high or low values
+for feature in log_data.keys():
+    
+    # TODO: Calculate Q1 (25th percentile of the data) for the given feature
+    Q1 = np.percentile(log_data[feature], 25)
+    
+    # TODO: Calculate Q3 (75th percentile of the data) for the given feature
+    Q3 = np.percentile(log_data[feature], 75)
+    
+    # TODO: Use the interquartile range to calculate an outlier step (1.5 times the interquartile range)
+    step = 1.5 * (Q3 - Q1)
+    
+    # Display the outliers
+    print("Data points considered outliers for the feature '{}':".format(feature))
+    display(log_data[~((log_data[feature] >= Q1 - step) & (log_data[feature] <= Q3 + step))])
+    
+# OPTIONAL: Select the indices for data points you wish to remove
+outliers  = [5, 10, 50, 150, 250, 300, 350, 400]
+
+# Remove the outliers, if any were specified
+good_data = log_data.drop(log_data.index[outliers]).reset_index(drop = True)
+
+from sklearn.decomposition import PCA
+
+# TODO: Apply PCA by fitting the good data with the same number of dimensions as features
+pca = PCA(n_components=6).fit(good_data)
+
+# TODO: Transform log_samples using the PCA fit above
+pca_samples = pca.transform(log_samples)
+
+# Generate PCA results plot
+pca_results = vs.pca_results(good_data, pca)
+
+# DataFrame of results
+display(pca_results)
+
+# DataFrame
+display(type(pca_results))
+
+# Cumulative explained variance should add to 1
+display(pca_results['Explained Variance'].cumsum())
+
+
+
+# Display sample log-data after having a PCA transformation applied
+display(pd.DataFrame(np.round(pca_samples, 4), columns = pca_results.index.values))
+
+# TODO: Apply PCA by fitting the good data with only two dimensions
+pca = PCA(n_components=2).fit(good_data)
+
+# TODO: Transform the good data using the PCA fit above
+reduced_data = pca.transform(good_data)
+
+# TODO: Transform log_samples using the PCA fit above
+pca_samples = pca.transform(log_samples)
+
+# Create a DataFrame for the reduced data
+reduced_data = pd.DataFrame(reduced_data, columns = ['Dimension 1', 'Dimension 2'])
+
+# Display sample log-data after applying PCA transformation in two dimensions
+display(pd.DataFrame(np.round(pca_samples, 4), columns = ['Dimension 1', 'Dimension 2']))
+
+# Create a biplot
+vs.biplot(good_data, reduced_data, pca)
+
+from sklearn.mixture import GMM
+from sklearn.metrics import silhouette_score
+
+for n_clusters in range(2,11):
+    # TODO: Apply your clustering algorithm of choice to the reduced data 
+    clusterer = GMM(n_components=n_clusters).fit(reduced_data)
+
+    # TODO: Predict the cluster for each data point
+    preds = clusterer.predict(reduced_data)
+
+    # TODO: Find the cluster centers
+    centers = clusterer.means_
+
+    # TODO: Predict the cluster for each transformed sample data point
+    sample_preds = clusterer.predict(pca_samples)
+
+    # TODO: Calculate the mean silhouette coefficient for the number of clusters chosen
+    score = silhouette_score(reduced_data, preds)
+    print("GMM num_clusters: {} - score: {}".format(n_clusters, score))
+    
+# Extra code because we ran a loop on top and this resets to what we want
+clusterer = GMM(n_components=3).fit(reduced_data)
+preds = clusterer.predict(reduced_data)
+centers = clusterer.means_
+sample_preds = clusterer.predict(pca_samples)
+
+# Display the results of the clustering from implementation
+vs.cluster_results(reduced_data, preds, centers, pca_samples)
+
+
+# TODO: Inverse transform the centers
+log_centers = pca.inverse_transform(centers)
+
+# TODO: Exponentiate the centers
+true_centers = np.exp(log_centers)
+
+# Display the true centers
+segments = ['Segment {}'.format(i) for i in range(0,len(centers))]
+true_centers = pd.DataFrame(np.round(true_centers), columns = data.keys())
+true_centers.index = segments
+display(true_centers)
+
+display(true_centers - data.median())  # Compare the centers with the median of the data
+
+display(true_centers - data.mean()) # Compare the centers with the mean of the data
+
+
